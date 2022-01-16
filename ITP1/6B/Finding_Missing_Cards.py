7bbd575283e288ed62a6d8e635d60c1268b57316
Keisuke Takada <ktaka@KeisukenoMacBook-Air.local>
2022-01-16 09:17:36 +0900

    Day8_in_Shinkansen

--- ITP1/6B/Finding_Missing_Cards.py
+++ ITP1/6B/Finding_Missing_Cards.py
@@ -1,6 +1,11 @@
 n = int(input())
-i = 0
 
-card = [input().split() for i in range(n)]
-sorted_card = sorted(card)
-print(sorted_card)
+hold_card = []
+for _ in range(n):
+	hold_suits, hold_num = input().split()
+	hold_num = int(hold_num)
+	hold_card.append((hold_suits, hold_num))
+all_card = [(suits, number) for suits in ["S", "H", "C", "D"] for number in range(1, 14)]
+for card in all_card:
+	if card not in hold_card:
+		print(*card)
