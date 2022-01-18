f4e58a946c4d470863bdcebf1e94b44773bca3dd
Ktaka95 <ktakada950711@gmail.com>
2022-01-19 00:05:49 +0900

    revive_contribution

--- ITP1/7A/Grading.py
+++ ITP1/7A/Grading.py
@@ -3,4 +3,21 @@ while True:
 	if m == -1 and f == -1 and r == -1:
 		break
 	else:
+		grading = 0
+		if m == -1 or f == -1:
+			grading = 'F'
+		elif m + f >= 80:
+			grading = 'A'
+		elif m + f >= 65:
+			grading = 'B'
+		elif m + f >= 50:
+			grading = 'C'
+		elif m + f >= 30:
+			if r >= 50:
+				grading = 'C'
+			else:
+				grading = 'D'
+		else:
+			grading = 'F'
+		print(grading)
 
