382a38b9db89a7f8e0a957868a09beec19f6ca02
Ktaka95 <ktakada950711@gmail.com>
2022-02-08 23:40:08 +0900

    Day27

--- ITP1/11B/Dice2.py
+++ ITP1/11B/Dice2.py
@@ -23,21 +23,37 @@ class Dice():
 		self.__top, self.__back, self.__bottom, self.__front = \
 		self.__back, self.__bottom, self.__front, self.__top
 
-	# def is_same_setting(self, ary):
-	# 	if self.top == ary[0] and self.front == ary[1], self.right == ary[2] and \
-	# 		self.left == arry[3] and self.back == ary[4] and self.bottom == ary[5]:
-	# 		return True
+	def spin_r(self):
+		self.__right, self.__front, self.__left, self.__back = \
+		self.__back, self.__right, self.__front, self.__left
 
-	def show_front(self):
-		return self.__front
+	def spin_l(self):
+		self.__right, self.__front, self.__left, self.__back = \
+		self.__front, self.__left, self.__back, self.__right
+
+	def is_same_top_front(self, ary):
+		if ary[0] == self.__top and ary[1] == self.__front:
+			return True
+
+	def top_front_right(self, ary):
+		right = 0
+		for _ in range(2):
+			for _ in range(3):
+				for _ in range(4):
+					if self.is_same_top_front(ary):
+						right = self.__right
+					self.spin_r()
+				self.turn_n()
+			self.spin_r()
+			self.turn_s()
+		return right
 
 number = list(map(int, input().split()))
 q = int(input())
-front_and_right = list(map(int(input().split()))
 
 dice = Dice(number)
 
 for i in range(q):
-
-
-print(dice.show_top())
+	top_and_front = list(map(int, input().split()))
+	right = dice.top_front_right(top_and_front)
+	print(right)
